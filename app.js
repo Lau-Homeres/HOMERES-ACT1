6a45e92d1c523d767276a7885429c1fad20acb12
Lau Homeres <laurencehomeres0@gmail.com>
2024-09-08 18:52:18 +0800

    Initial commit

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 console.log ("Hello World");
-let name = "Lurence Homerez"
+let name =  "Lurence Homerez"
 console.log ("My name is" +  name);
+
+console.log('-Using Variables-');
+let Name = "Lurence Homerez";
+let age = "19 ";
+console.log ("Sawadeeka! I am " + Name + ", " + age + "years old.");
+
+
+console.log("-Using Numbers-");
+const V = 1;
+const A = 2;
+
+const sum = V + A;
+console.log(sum);
+
+console.log('-using Booleans-');
+let L = true;
+let O = false;
+console.log("Programming is so easy,","It's", O, ".");
+console.log("I am encouraging myself in programming,","It's", L,".");
+
+console.log('-Comparison Operators Sections-');
+let R = 1;
+let I = 2;
+
+let E = R>I;
+console.log("Is i more than r?"+" "+ E);
+let G = I<R;
+console.log("Is r more than i?"+" "+ G);
+let M = R === I;
+console.log("Is r equal to i?"+" "+ M);
+let N =I != R;
+console.log("Is i not equal to r?"+" "+ N);
